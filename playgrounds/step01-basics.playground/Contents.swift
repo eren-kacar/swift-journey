3aa7f5541a12d8acb6af24271564c049152a96a1
u240709002-admin <erenkacarr04@icloud.com>
2026-07-24 19:49:09 +0300

    Complete Step 1: Swift basics

---
+++ playgrounds/step01-basics.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+let name = "Eren"
+let age = 22
+let university = "MSKU"
+let department = "Computer Engineering"
+let height = 1.77
+let wantsToBeEngineer = true
+
+
+//print("Hello! My name is \(name).")
+//print("I am \(age) years old.")
+//print("I study \(department) at \(university).")
+//print("My height is \(height) meters.")
+//print("Do I want to become an engineer? \(wantsToBeEngineer)")
+
+let favoriteLanguage = "Swift"
+let favoriteDevice = "MBA"
+let learningGoal = "Build modern iOS apps"
+let favoriteFood = "Hamburger"
+let favoriteMovie = "Star Wars III"
+let isStudent = true
+
+//print("My favorite programming language \(favoriteLanguage).")
+//print("I like to use \(favoriteDevice).")
+//print("I want to learn \(learningGoal).")
+
+let city = "Muğla"
+
+//print(type(of: city))
+//print(type(of: age))
+//print(type(of: height))
+//print(type(of: wantsToBeEngineer))
+
+//print("===== PROFILE =====")
+//print("Name: \(name)")
+//print("Age: \(age)")
+//print("Favorite Food: \(favoriteFood)")
+//print("Favorite Movie: \(favoriteMovie)")
+//print("Student \(isStudent)")
+//print("===================")
